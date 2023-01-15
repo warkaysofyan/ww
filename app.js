770bb8c6e7e43bcdd41abe1cc4fc2677b266e6ac
warkaysofyan <mrwarkay@gmail.com>
2023-01-15 18:28:02 +0000

    re ordring files

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 let morgan = require('morgan');
 let app = express();
 app.use(morgan('dev'));
-
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 let port = process.env.PORT || 5000;
 
-app.use('/', (t, res) => {
-    console.log(t.ips);
-    res.send('no datat');
-});
+// ! implemanting routes :
+let lPage = require('./routes/users/lanPage');
+
+app.use('/', lPage);
 
 app.listen(port, () => {
     console.log('app startin on port ' + port);
